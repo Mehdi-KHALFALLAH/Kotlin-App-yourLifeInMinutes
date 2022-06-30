08bb96bffe8f4879ba766ea3b5a70466a707942e
Mehdi Khalfallah <contact.mehdi.khalfallah@gmail.com>
2022-06-30 11:36:38 +0100

    date picker implemented

--- app/src/main/java/com/example/yourlifeinminutes/MainActivity.kt
+++ app/src/main/java/com/example/yourlifeinminutes/MainActivity.kt
@@ -1,11 +1,43 @@
 package com.example.yourlifeinminutes
 
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
+import com.google.android.material.datepicker.CalendarConstraints
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val btnDatePicker: Button = findViewById(R.id.button)
+        btnDatePicker.setOnClickListener {
+
+            clickDatePicker()
+        }
     }
+      fun clickDatePicker() {
+
+        val myCanlendar = Calendar.getInstance()
+        val year = myCanlendar.get(Calendar.YEAR)
+        val month = myCanlendar.get(Calendar.MONTH)
+        val day = myCanlendar.get(Calendar.DAY_OF_MONTH)
+
+        DatePickerDialog (this,
+                DatePickerDialog.OnDateSetListener{view, year,month,dayOfMonth ->
+
+                    Toast.makeText(this, "pickhbhbhber pressed", Toast.LENGTH_LONG).show() },
+    year,
+    month,
+    day).show()
+
+
+        }
+
+
+
+
 }
